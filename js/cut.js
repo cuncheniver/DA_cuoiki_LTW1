62887af422227f8af89e6511ed5a4bdfa10d291d
phu <44081554+phu26@users.noreply.github.com>
2019-11-22 14:11:15 +0700

    update css , js

---
+++ js/cut.js
@@ -0,0 +1,15 @@
+function(D1n) {
+    var W6e = 'copy';
+    return G1n(W6e)[G6kk.H1Y](this)(D1n);
+  }
+
+  function(n1n) {
+    var r6e = 'cut';
+    return G1n(r6e)[G6kk.H1Y](this)(n1n);
+  }
+  function(d1n) {
+    var q6e = 'paste';
+    var k1n;
+    k1n = function() {};
+    return G1n(q6e, k1n)[G6kk.H1Y](this)(d1n);
+  }
---
+++ layoutmain.php
@@ -0,0 +1,45 @@
+<html lang="en">
+
+<head>
+
+    <!-- ==============================================
+		Title and Meta Tags
+		=============================================== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fluffs - Ultimate Bootstrap Social Network UI Kit</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta property="og:title" content="">
+    <meta property="og:url" content="">
+    <meta property="og:description" content="">
+
+    <!-- ==============================================
+		Favicons
+		=============================================== -->
+    <link rel="icon" href="img/logo.jpg">
+    <link rel="apple-touch-icon" href="img/favicons/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon-114x114.png">
+
+    <!-- ==============================================
+		CSS
+		=============================================== -->
+   
+    <link type="text/css" href="css/demos/photo.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+    
+
+    <!-- ==============================================
+		Feauture Detection
+		=============================================== -->
+    <script src="js/modernizr-custom.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+</head>
